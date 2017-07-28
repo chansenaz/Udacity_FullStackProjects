55d816ba04a21ed692d3685bad9c67520984a31d
Christopher Hansen <chansen2@asu.edu>
2017-07-28 00:40:50 -0700

    p3 - first commit

---
+++ p3_itemcatalog/item_catalog/database_setup.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+
+class Item(Base):
+    __tablename__ = 'items'
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+
+Base.metadata.create_all(engine)
